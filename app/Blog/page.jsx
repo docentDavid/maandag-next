5cfc6cdfe191f1e67f4a6c9f7859676ac377533f
David Schol <d.schol@fontys.nl>
2025-02-17 15:46:01 +0100

    Enhance Blog page loading state and format post dates; update global styles for improved layout and typography

--- app/Blog/page.jsx
+++ app/Blog/page.jsx
@@ -25,15 +25,35 @@ export default function Blog() {
     fetchData();
   }, []);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading)
+    return (
+      <main>
+        <div className="flex flex-col justify-center items-center gap-[3rem]">
+          <div className="block">Loading...</div>
+          <div className="animate-spin rounded-full h-10 w-10 border-t-4 border-gray-500"></div>
+        </div>
+      </main>
+    );
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString("nl-NL", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+  };
 
   return (
     <main className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       {blogPosts.map((post) => (
         <div
-          className="card bg-white shadow-lg rounded-lg p-4 flex flex-col h-full"
+          className="card bg-white shadow-lg rounded-lg p-4 flex flex-col h-full relative"
           key={post.id}
         >
+          <div className="absolute top-4 right-4 text-sm text-gray-500">
+            {formatDate(post.date)}
+          </div>
           <h2 className="text-[20px] font-bold mb-4">{post.title}</h2>
           <p className="text-base my-4">{post.description}</p>
           <div className="mt-auto flex justify-end">
